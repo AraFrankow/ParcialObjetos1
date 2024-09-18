e2b61752564bd6f5e49b93296b744e4cc211f4a3
AraFrankow <ara.frankow@gmail.com>
2024-09-18 14:53:44 -0300

    2

--- src/Ejercicio1Vet.java
+++ src/Ejercicio1Vet.java
@@ -1,8 +1,23 @@
+import javax.swing.JOptionPane;
 
 public class Ejercicio1Vet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Veterinaria clientes = new Veterinaria();
+		String nombreDueño;
+		do {
+			nombreDueño = JOptionPane.showInputDialog("Ingrese el nombre del dueño");
+		} while (!clientes.ValidarNombre(nombreDueño));
+		String nombreMascota;
+		do {
+			nombreMascota = JOptionPane.showInputDialog("Ingrese el nombre del animal");
+		} while (!clientes.ValidarNombre(nombreMascota));
+		String motivo = JOptionPane.showInputDialog("Ingrese el motivo de su consulta");
+		
+		String[] menu = {"Esta todo bien","Necesita venir dentro de poco"};
+		int estado= JOptionPane.showOptionDialog(null, "Como se encuentra",null,0,0,null, menu, menu[0]);
+		int vacunas= JOptionPane.showOptionDialog(null, "Como estan sus vacunas?",null,0,0,null, menu, menu[0]);
+		JOptionPane.showMessageDialog(null, clientes);
 
 	}
 
--- src/Veterinaria.java
+++ src/Veterinaria.java
@@ -9,17 +9,31 @@ public class Veterinaria {
 		private String motivo;
 		private boolean estado;
 		private LocalDate cosultaProxima;
+		private double peso;
+		private boolean vacunas;
 		
-		public Veterinaria(String nombreAnimal, String nombreDueño, LocalDate cosulta, String motivo, boolean estado, LocalDate cosultaProxima) {
+		public Veterinaria(String nombreAnimal, String nombreDueño, LocalDate cosulta, String motivo, boolean estado, LocalDate cosultaProxima, int peso, boolean vacunas) {
 			this.nombreAnimal=nombreAnimal;
 			this.nombreDueño=nombreDueño;
 			this.cosulta=cosulta;
 			this.motivo=motivo;
 			this.estado=estado;
 			this.cosultaProxima=cosultaProxima;
+			this.peso=peso;
+			this.vacunas=vacunas;
+		}
+		public Veterinaria() {
+			this.nombreAnimal="sin nombre";
+			this.nombreDueño="sin nombre";
+			this.cosulta=LocalDate.now();
+			this.motivo="sin motivo";
+			this.estado=false;
+			this.cosultaProxima=LocalDate.now();
+			this.peso=0;
+			this.vacunas=false;
 		}
 		
-		public static boolean ValidarNombre(String nombre) {
+		public boolean ValidarNombre(String nombre) {
 			boolean numero = false;
 			if (nombre.isEmpty()) {
 				JOptionPane.showMessageDialog(null, "Está vacio el nombre");
@@ -41,10 +55,36 @@ public class Veterinaria {
 		
 		public String mostrarEstado(boolean estado) {
 			if (estado==true) {
-				return "Se encuentra bien";
+				return "Esta todo bien";
 			} else {
-				return "Necesita ser revisado dentro de poco";
+				return "Necesita venir dentro de poco";
 			}
 
+		}
+		public int validarNumeros(String numero) {
+			boolean numer ;
+			String num ="" ;
+			do {
+				numer =true;
+				num = JOptionPane.showInputDialog(numero);
+				while (num.isEmpty()) {
+					num = JOptionPane.showInputDialog(numero);
+				}
+				for (int i = 0; i < num.length(); i++) {
+					if (!Character.isDigit(num.charAt(i))) {
+						numer = false;
+						break;
+					}
+				}
+			} while (!numer);
+			
+			return Integer.parseInt(num);
+		}
+
+		@Override
+		public String toString() {
+			return "Veterinaria [nombreAnimal=" + nombreAnimal + ", nombreDueño=" + nombreDueño + ", cosulta=" + cosulta
+					+ ", motivo=" + motivo + ", estado=" + mostrarEstado(estado) + ", cosultaProxima=" + cosultaProxima + ", peso="
+					+ peso + ", vacunas=" + mostrarEstado(vacunas) + "]";
 		}
 }
